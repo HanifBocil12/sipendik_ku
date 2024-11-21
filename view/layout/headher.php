7c0292b0e53a6549fb408208c6afbaa64078b91f
rehann33w2 <hakimrehannygy@gmail.com>
2024-11-21 10:22:42 +0700

    10:22

--- view/layout/headher.php
+++ view/layout/headher.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
-<body>
-
+<body class="inter-regular">
+    as
 </body>
 
 </html>
