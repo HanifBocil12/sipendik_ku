eb6c6471455b357f4d56c3bbe65885f8ad2279b3
ansfjk <botf7060@gmial.com>
2024-11-23 20:40:21 +0700

    jam 8

--- view/index.php
+++ view/index.php
@@ -1,4 +1,25 @@
 <?php 
-    include 'layout/headher.php'
-    include 'partials/navbar.php'
+    include 'layout/headher.php';
+    include 'partials/navbar.php';
 ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <?= include 'partials/sidebar.php' ?>
+        </div>
+        <div class="col-md-9">
+            <?php 
+            $page = isset($_GET['page']) ?: 'dashboard';
+            switch ($page) {
+                case 'dashboard':
+                    # code...
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+            ?>
+        </div>
+    </div>
+</div>
