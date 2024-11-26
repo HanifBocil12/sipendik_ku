bf35f1899ca4ecead6385d0f29ea2eaebf26cce8
rehann33w2 <hakimrehannygy@gmail.com>
2024-11-26 11:42:19 +0700

    ss

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <div class="container">
     <div class="row">
         <div class="col-3">
-
+            <?php include 'partials/sidebar.php';
+            ?>
         </div>
         <div class="col-9">
             <?php 
@@ -16,6 +17,15 @@
                     break;
                     case 'students':
                         include 'pages/students.php';
+                        break;
+                    case 'add-student':
+                        include 'pages/add-student';
+                        break;
+                    case 'detail-student':
+                        include 'pages/detail-student';
+                        break;
+                    case 'edit-student':
+                        include 'pages/edit-student';
                         break;
                 }
             ?>
